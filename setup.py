f682a50e41407cbd050a8c4bfe17c399bd8d87bd
Daniel Stonier <d.stonier@gmail.com>
2017-02-16 13:09:40 +0900

    0.5.15

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ prefix = userbase or opts.prefix or sys.prefix
 
 setup(
     name='ckx_tools',
-    version='0.5.14',
+    version='0.5.15',
     packages=find_packages(exclude=['tests*', 'docs*']),  # ['ckx_tools'],  # find_packages(exclude=['tests', 'docs']), <-- broken, it picks up sub-packages, e.g. tests.unit
     package_data={
         'ckx_tools': [
